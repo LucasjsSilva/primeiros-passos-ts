4c09ddf5d56eaab4e0ed99ce862e1a7f611fbb72
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-16 19:41:33 -0300

    implementando subclass

--- build/index.js
+++ build/index.js
@@ -9,6 +9,13 @@ class Character {
         console.log(`${this.name} atack with ${this.stregth} points`);
     }
 }
+class Magician extends Character {
+    constructor(name, stregth, skill, magicPoints) {
+        super(name, stregth, skill);
+        this.magicPoints = magicPoints;
+    }
+}
 const p1 = new Character("Magikarp", 10, 11);
+const p2 = new Magician("Patolino", 5, 10, 1000);
 console.log(p1);
 p1.atack();
--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,29 @@ class Character {
     protected stregth: number;
     private skill: number;
 
-    constructor(name:string, stregth: number, skill: number){
+    constructor(name: string, stregth: number, skill: number) {
         this.name = name;
         this.stregth = stregth;
         this.skill = skill;
     }
 
-    atack(): void{
+    atack(): void {
         console.log(`${this.name} atack with ${this.stregth} points`);
     }
 }
 
+//superclass: Character
+//subclass: Magician
+
+class Magician extends Character {
+    magicPoints: number;
+    constructor(name: string, stregth: number, skill: number, magicPoints: number) {
+        super(name, stregth, skill);
+        this.magicPoints = magicPoints;
+    }
+}
+
 const p1 = new Character("Magikarp", 10, 11);
+const p2 = new Magician("Patolino",5,10,1000)
 console.log(p1);
 p1.atack();
