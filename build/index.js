2e8445c9379944eb0cc115beba01d4564d4e77d6
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-16 20:03:00 -0300

    utilizando o ts-node-dev para melhor desenvolvimento

--- build/index.js
+++ build/index.js
@@ -1,8 +1,1 @@
 "use strict";
-function concatArrays(...itens) {
-    return new Array().concat(...itens);
-}
-const numArray = concatArrays([1, 2, 3], [4]);
-const stgArray = concatArrays(["Lucas", "Jesus"], ["Santos", "Silva"]);
-console.log(numArray);
-console.log(stgArray);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-//Generics
-function concatArrays<T>(...itens: T[]): T[]{
-    return new Array().concat(...itens);
-}
-
-const numArray = concatArrays<number[]>([1, 2, 3], [4]);
-const stgArray = concatArrays<string[]>(["Lucas", "Jesus"], ["Santos", "Silva"]);
-
-console.log(numArray);
-console.log(stgArray);
+let dado: string = "Joao";
+console.log(dado);
