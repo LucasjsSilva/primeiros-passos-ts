8d7de935d28360bdef29ab472ae82ea3403a4eef
LucasjsSilva <luquinahjssilva@ufpi.edu.br>
2024-08-16 19:31:24 -0300

    Data modifiers

--- build/index.js
+++ build/index.js
@@ -1,13 +1,14 @@
 "use strict";
 class Character {
-    constructor(stregth, skill) {
+    constructor(name, stregth, skill) {
+        this.name = name;
         this.stregth = stregth;
         this.skill = skill;
     }
     atack() {
-        console.log(`Atack with ${this.stregth} points`);
+        console.log(`${this.name} atack with ${this.stregth} points`);
     }
 }
-const p1 = new Character(10, 11);
+const p1 = new Character("Magikarp", 10, 11);
 console.log(p1);
 p1.atack();
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 //classes
-class Character {
-    name?: string;
-    stregth: number;
-    skill: number;
 
-    constructor(stregth: number, skill: number){
+/**
+ * Data modifiers
+ * Public(padrao)
+ * protect
+ * private
+ */
+
+class Character {
+    public name: string;
+    protected stregth: number;
+    private skill: number;
+
+    constructor(name:string, stregth: number, skill: number){
+        this.name = name;
         this.stregth = stregth;
         this.skill = skill;
     }
 
     atack(): void{
-        console.log(`Atack with ${this.stregth} points`);
+        console.log(`${this.name} atack with ${this.stregth} points`);
     }
 }
 
-const p1 = new Character(10, 11);
+const p1 = new Character("Magikarp", 10, 11);
 console.log(p1);
 p1.atack();
