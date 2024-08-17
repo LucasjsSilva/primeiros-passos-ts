8db00d1e71a9597e1b78feb7665fab724f109b3b
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-08-16 23:01:42 -0300

    implementando atribute decorator

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,40 @@
 //dacorators
+//class dacorator
 function apiVersion(version: string){
     return (target: any) => {
         Object.assign(target.prototype, {__version: version, __name: "Lucas"})
     } 
 }
 
-@apiVersion("1.0")
-class Api{}
+//atribuute dacorator
+function minLength(lenght: number){
+    return (target: any, key: string) => {
+        let _value = target[key];
 
-const api = new Api();
-console.log(api.__version)
+        const getter = () => _value;
+        const setter = (value: string) => {
+            if (value.length < lenght){
+                throw new Error(`Tamanho menor que ${lenght}`);
+            }else {
+                _value = value;
+            }
+        }
+
+        Object.defineProperty(target, key,{
+            get: getter,
+            set: setter,
+        })
+    }
+}
+
+class Api{
+    @minLength(3)
+    name: string;
+
+    constructor(name:string){
+        this.name = name;
+    }
+}
+
+const api = new Api("pro");
+console.log(api.name)
